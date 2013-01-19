82e216946c82e36d90ca381284643db2a5cfab1a
Philippe <pmiriello@gmail.com>
2013-01-19 13:41:46 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -19,6 +19,7 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
+import javax.swing.JTextField;
 
 
 public class Application extends JFrame {
@@ -46,6 +47,8 @@ public class Application extends JFrame {
 	private JLabel lblKg_1;
 	private JLabel lblMs;
 	private JLabel lblO;
+	private JTextField textVitesse;
+	private JTextField textDeplace;
 
 	/**
 	 * Launch the application.
@@ -74,13 +77,13 @@ public class Application extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel.setBounds(11, 342, 399, 129);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		btnCorrection = new JButton("Correction");
 		btnCorrection.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -99,27 +102,27 @@ public class Application extends JFrame {
 		});
 		btnCorrection.setBounds(230, 84, 107, 23);
 		panel.add(btnCorrection);
-		
+
 		lblMasse1 = new JLabel("Masse 1");
 		lblMasse1.setBounds(10, 11, 82, 14);
 		panel.add(lblMasse1);
-		
+
 		lblMasse2 = new JLabel("Masse 2");
 		lblMasse2.setBounds(200, 11, 82, 14);
 		panel.add(lblMasse2);
-		
+
 		lblCoeff = new JLabel("Coeff");
 		lblCoeff.setBounds(10, 50, 46, 14);
 		panel.add(lblCoeff);
-		
+
 		lblNewLabel_3 = new JLabel("Angle (Maximum 30)");
 		lblNewLabel_3.setBounds(200, 50, 117, 14);
 		panel.add(lblNewLabel_3);
-		
+
 		lblVitesse = new JLabel("Vitesse");
 		lblVitesse.setBounds(10, 88, 82, 14);
 		panel.add(lblVitesse);
-		
+
 		spnMasse1 = new JSpinner();
 		spnMasse1.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
@@ -129,7 +132,7 @@ public class Application extends JFrame {
 		spnMasse1.setModel(new SpinnerNumberModel(new Integer(100), new Integer(100), null, new Integer(10)));
 		spnMasse1.setBounds(66, 8, 87, 20);
 		panel.add(spnMasse1);
-		
+
 		spnMasse2 = new JSpinner();
 		spnMasse2.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
@@ -139,7 +142,7 @@ public class Application extends JFrame {
 		spnMasse2.setModel(new SpinnerNumberModel(new Integer(100), new Integer(100), null, new Integer(10)));
 		spnMasse2.setBounds(253, 8, 113, 20);
 		panel.add(spnMasse2);
-		
+
 		spnCoeff = new JSpinner();
 		spnCoeff.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
@@ -149,17 +152,17 @@ public class Application extends JFrame {
 		spnCoeff.setModel(new SpinnerNumberModel(new Double(1), new Double(0), null, new Double(0.2)));
 		spnCoeff.setBounds(66, 47, 87, 20);
 		panel.add(spnCoeff);
-		
+
 		spnVitesse = new JSpinner();
 		spnVitesse.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
 				simulation.setV1((double) spnVitesse.getValue());
 			}
 		});
-		spnVitesse.setModel(new SpinnerNumberModel(new Double(1.0), new Double(1.0), null, new Double(1.0)));
+		spnVitesse.setModel(new SpinnerNumberModel(new Double(35), new Double(1), null, new Double(5)));
 		spnVitesse.setBounds(66, 85, 87, 20);
 		panel.add(spnVitesse);
-		
+
 		spnAngle = new JSpinner();
 		spnAngle.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
@@ -169,29 +172,29 @@ public class Application extends JFrame {
 		spnAngle.setModel(new SpinnerNumberModel(10.0, 0.0, 30.0, 1.0));
 		spnAngle.setBounds(327, 47, 39, 20);
 		panel.add(spnAngle);
-		
+
 		lblKg = new JLabel("kg");
 		lblKg.setBounds(158, 10, 52, 16);
 		panel.add(lblKg);
-		
+
 		lblKg_1 = new JLabel("kg");
 		lblKg_1.setBounds(369, 10, 52, 16);
 		panel.add(lblKg_1);
-		
+
 		lblMs = new JLabel("m/s");
 		lblMs.setBounds(158, 87, 52, 16);
 		panel.add(lblMs);
-		
+
 		lblO = new JLabel("o");
 		lblO.setBounds(369, 41, 52, 16);
 		panel.add(lblO);
-		
+
 		panel_1 = new JPanel();
 		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_1.setBounds(611, 342, 400, 129);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		btnQuittez = new JButton("Quittez");
 		btnQuittez.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -200,13 +203,26 @@ public class Application extends JFrame {
 		});
 		btnQuittez.setBounds(301, 95, 89, 23);
 		panel_1.add(btnQuittez);
-		
+
+		textVitesse = new JTextField();
+		textVitesse.setEditable(false);
+		textVitesse.setBounds(10, 39, 174, 62);
+		panel_1.add(textVitesse);
+		textVitesse.setColumns(10);
+
+		textDeplace = new JTextField();
+		textDeplace.setEditable(false);
+		textDeplace.setText("");
+		textDeplace.setBounds(213, 39, 86, 20);
+		panel_1.add(textDeplace);
+		textDeplace.setColumns(10);
+
 		panel_2 = new JPanel();
 		panel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_2.setBounds(420, 342, 182, 129);
 		contentPane.add(panel_2);
 		panel_2.setLayout(null);
-		
+
 		btnStop = new JButton("Stop");
 		btnStop.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -217,7 +233,7 @@ public class Application extends JFrame {
 		});
 		btnStop.setBounds(20, 82, 140, 23);
 		panel_2.add(btnStop);
-		
+
 		tglPlayPause = new JToggleButton("Play");
 		tglPlayPause.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -232,8 +248,22 @@ public class Application extends JFrame {
 		});
 		tglPlayPause.setBounds(10, 11, 162, 60);
 		panel_2.add(tglPlayPause);
-		
+
 		simulation = new Simulation();
+		simulation.addSimListener(new SimListener() {
+			public void angleChange() {
+				spnAngle.setValue(simulation.getAngle());
+			}
+			public void animationTermine() {
+			}
+			public void estAnime() {
+
+
+				textVitesse.setText(Double.toString(simulation.getV2()));
+				textDeplace.setText(Double.toString(simulation.getDeplace()));
+
+			}
+		});
 		simulation.setBorder(null);
 		simulation.setBounds(11, 11, 1000, 350);
 		simulation.setAngle(20);
