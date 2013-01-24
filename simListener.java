f87570cddcdde9df79de5fa66fa239a0ce0759cd
Konstantin <fedorov.konstantin@gmail.com>
2013-01-24 10:11:04 -0500

    Update simListener.java

--- simListener.java
+++ simListener.java
@@ -2,7 +2,8 @@ import java.util.EventListener;
 
 
 public interface SimListener extends EventListener{
-  public void estAnime();
+	public void animationDebut();
+	public void estAnime();
 	public void animationTermine();
 	public void angleChange();
 }
