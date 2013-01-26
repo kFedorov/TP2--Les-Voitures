95c7d06b06548799be42c46495b70750d2b2c8df
Philippe <pmiriello@gmail.com>
2013-01-26 13:15:15 -0500

    Resize fonctionne parfaitement

--- FeuCirculation.java
+++ FeuCirculation.java
@@ -3,7 +3,6 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.RenderingHints;
 import java.io.IOException;
 import java.net.URL;
 
@@ -13,12 +12,10 @@ import javax.swing.JPanel;
 
 public class FeuCirculation extends JPanel {
 
-	private static final long serialVersionUID = 1L;
 	private Image feuCirc;
 	private Color feu=Color.red;
-	private Color rOff=Color.red.darker(), vOff=Color.green.darker(), jOff=Color.yellow.darker();
 	private double factX;
-	private int alY=(int)(factX*85);
+	private int alY;
 	private boolean drawFeu = false;
 
 
@@ -27,7 +24,7 @@ public class FeuCirculation extends JPanel {
 	 */
 	public FeuCirculation() {
 		setOpaque(false);
-		setPreferredSize(new Dimension(300, 300));
+		setPreferredSize(new Dimension(400, 400));
 		URL fichierFeu= getClass().getClassLoader().getResource("feuCirculation.gif");
 		try {
 			feuCirc=ImageIO.read(fichierFeu);
@@ -44,23 +41,25 @@ public class FeuCirculation extends JPanel {
 
 		super.paintComponent(g);
 		Graphics2D g2d=(Graphics2D)g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
 		g2d.drawImage(feuCirc, 0, 0, getWidth(), getHeight(), null);
+		factX=((double)getHeight()/400);
 
-		g2d.setColor(rOff);
-		g2d.fillOval((int) (factX*171),(int) (factX* 112),(int) (factX* 52), (int) (factX*52));
+		g2d.setColor(Color.red.darker());
+		g2d.fillOval((int) (factX*170),(int) (factX*113),(int) (factX*55), (int) (factX*55));
 
-		g2d.setColor(jOff);
-		g2d.fillOval((int) (factX*171), (int) (factX*177),(int) (factX* 52),(int) (factX* 52));
+		g2d.setColor(Color.yellow.darker());
+		g2d.fillOval((int) (factX*170), (int) (factX*178),(int) (factX*55),(int) (factX*55));
 
-		g2d.setColor(vOff);
-		g2d.fillOval((int) (factX*171), (int) (factX*241), (int) (factX*52), (int) (factX*52));
+		g2d.setColor(Color.green.darker());
+		g2d.fillOval((int)(factX*170), (int)(factX*242), (int)(factX*55), (int)(factX*55));
 
 		if(drawFeu){
 			feu=feu.brighter();
 			feu=feu.brighter();
 			g2d.setColor(feu);
-			g2d.fillOval((int)(factX*171), alY, (int) (factX*52), (int) (factX*52));
+			g2d.fillOval((int)(factX*(double)170), alY, (int)(factX*55),(int)(factX*55));
+			System.out.println(alY);
 		}
 
 
@@ -71,12 +70,10 @@ public class FeuCirculation extends JPanel {
 	public void setBounds(int x, int y, int width, int height){
 		if (width>height){
 			super.setBounds(x,y,width,width);
-			factX=(double)(width)/400;
 		}else{
 			super.setBounds(x,y,height,height);
-			factX=(double)(height)/400;
 		}
-		
+		repaint();
 	}
 
 
@@ -86,11 +83,11 @@ public class FeuCirculation extends JPanel {
 
 	public void setFeu(Feu couleur) {
 		switch(couleur){
-		case Vert: alY= (int)(factX*241); feu=Color.green; drawFeu=true;
+		case Vert: alY= (int)(factX*242); feu=Color.green; drawFeu=true;
 		break;
-		case Jaune: alY=(int)(factX*177); feu=Color.yellow; drawFeu=true;
+		case Jaune: alY=(int)(factX*178); feu=Color.yellow; drawFeu=true;
 		break;
-		case Rouge: alY=(int)(factX*112); feu=Color.red; drawFeu=true;
+		case Rouge: alY=(int)(factX*113); feu=Color.red; drawFeu=true;
 		break;
 		case Aucune: drawFeu=false;
 		break;
